edec808659cb748ec44705b36e798b5076a44281
Rafael Missio <rtmissio@gmail.com>
2024-04-14 21:45:05 -0300

    Criando o Produtor e Consumidor Kafka

---
+++ Consumidor/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Confluent.Kafka;
+
+var config = new ConsumerConfig{
+    GroupId = "missio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe("topico-teste");
+
+while(true){
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Key} - {result.Message.Value}");
+}
