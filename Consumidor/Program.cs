6c27c0b76e03ee2d7051f63829e132206f7f74c4
Rafael Missio <rtmissio@gmail.com>
2024-04-15 17:06:41 -0300

    implementação do schema registry comsumidor

--- Consumidor/Program.cs
+++ Consumidor/Program.cs
@@ -1,15 +1,26 @@
 ﻿
 using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ConsumerConfig{
     GroupId = "missio",
     BootstrapServers = "localhost:9092"
 };
 
-using var consumer = new ConsumerBuilder<string, string>(config).Build();
-consumer.Subscribe("topico-teste");
+using var consumer = new ConsumerBuilder<string, missio.Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<missio.Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
+consumer.Subscribe("cursos");
 
 while(true){
     var result = consumer.Consume();
-    Console.WriteLine($"Mensagem: {result.Message.Key} - {result.Message.Value}");
+    Console.WriteLine($"Mensagem: {result.Message.Value.Descricao}");
 }
--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -11,21 +11,23 @@ var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ProducerConfig{BootstrapServers = "localhost:9092"};
 
-using var producer = new ProducerBuilder<string,string>(config)
-//.SetValueSerializer(new AvroSerializer<missio.Curso>(schemaRegistry))
-//.SetValueSerializer(new AvroSerializer<missio.Curso>(config))
+using var producer = new ProducerBuilder<string,missio.Curso>(config)
+.SetValueSerializer(new AvroSerializer<missio.Curso>(schemaRegistry))
 .Build();
-
 
 var message = new Message<string, missio.Curso>
 {
     Key = Guid.NewGuid().ToString(),
-    Value = new missio.Curso
-    {
+    Value = new missio.Curso{
         Id = Guid.NewGuid().ToString(),
-        Descricao = "Teste"
+        Descricao = "Kafka Teste"
     }
+
 };
 
-//var result = await producer.ProduceAsync("topico-teste", message);
-//Console.WriteLine($"{result.Offset}");
+
+
+var result = await producer.ProduceAsync("cursos", message);
+
+
+Console.WriteLine($"{result.Offset}");
