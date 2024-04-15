0b1b36479a6cd5050b26e6230d6cba387f5f6408
Rafael Missio <rtmissio@gmail.com>
2024-04-15 16:40:29 -0300

    implementando SchemaRegistry no Pordutor

--- Consumidor/obj/Debug/net8.0/Consumidor.AssemblyInfo.cs
+++ Consumidor/obj/Debug/net8.0/Consumidor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consumidor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6466d620171b3afacb636657657a90dcfb6a41b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e70092a4d66458468a62841fe0b15c8e6573ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consumidor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consumidor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -1,15 +1,31 @@
 ﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
 
-var config = new ProducerConfig{BootstrapServers = "localhost:9092"};
-using var producer = new ProducerBuilder<string,string>(config).Build();
 
-
-var message = new Message<string, string>
-{
-    Key = Guid.NewGuid().ToString(),
-    Value = $"Mensagem teste {DateTime.Now.Second}"
-
+var schemaConfig = new SchemaRegistryConfig{
+    Url = "http://localhost:8081"
 };
 
-var result = await producer.ProduceAsync("topico-teste",message);
-Console.WriteLine($"{result.Offset}");
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig{BootstrapServers = "localhost:9092"};
+
+using var producer = new ProducerBuilder<string,string>(config)
+//.SetValueSerializer(new AvroSerializer<missio.Curso>(schemaRegistry))
+//.SetValueSerializer(new AvroSerializer<missio.Curso>(config))
+.Build();
+
+
+var message = new Message<string, missio.Curso>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new missio.Curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = "Teste"
+    }
+};
+
+//var result = await producer.ProduceAsync("topico-teste", message);
+//Console.WriteLine($"{result.Offset}");
